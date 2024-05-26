79f693e14e929e1e01847508793fe8083f2362e6
Jonathan Marin <marinjonathan40@gmail.com>
2024-05-25 19:19:35 -0500

    Inicio trabajo en migraciones

--- routes/api.php
+++ routes/api.php
@@ -1,30 +1,53 @@
 <?php
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\api\AuthController;
+use App\Http\Controllers\api\FallbackController;
 use App\Http\Controllers\api\UserController;
 use App\Http\Controllers\api\NoteController;
 use App\Http\Controllers\api\RolController;
 use App\Http\Controllers\api\StateController;
 use App\Http\Controllers\api\VisibilityController;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 
 
 
-// Generación de rutas ---------------------------------------------
-Route::resources([
+// Generacion de rutas individuales -----------------------------------------------------------------------------------
 
-    // generacion de rutas para users
-    'user' => UserController::class,
+Route::post('register', [AuthController::class, 'store']);
 
-    // generacion de rutas para notes
-    'note' => NoteController::class,
+Route::post('login', [AuthController::class, 'login']);
 
-    // generacion de rutas para rols
-    'rol' => RolController::class,
 
-    // generacion de rutas para states
-    'state' => StateController::class,
 
-    // generacion de rutas para visibility
-    'visibility' => VisibilityController::class
-]);
+
+Route::group([
+    "middleware" => ["auth:sanctum"]
+], function () {
+
+
+    // Generación de rutas --------------------------------------------------------------------------------------------------
+    Route::resources([
+
+        // generacion de rutas para users
+        'user' => UserController::class,
+
+        // generacion de rutas para notes
+        'note' => NoteController::class,
+
+        // generacion de rutas para rols
+        'rol' => RolController::class,
+
+        // generacion de rutas para states
+        'state' => StateController::class,
+
+        // generacion de rutas para visibility
+        'visibility' => VisibilityController::class
+    ]);
+});
+
+
+// Manejo de rutas inexistentes y mal manejo de metodos -----------------------------------------------------------------------------------------
+
+Route::any('{fallbackPlaceholder?}', [FallbackController::class, 'fallback'])
+    ->where('fallbackPlaceholder', '.*');
