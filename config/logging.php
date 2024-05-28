9db1940bcd1fccbee2e64fcc60ae5f0bc8e3044a
Jonathan Marin <marinjonathan40@gmail.com>
2024-05-28 18:00:44 -0500

    Manejo de log
    logs en archivo separados por días y 7  días de retención de esos archivos

--- config/logging.php
+++ config/logging.php
@@ -67,9 +67,10 @@ return [
 
         'daily' => [
             'driver' => 'daily',
-            'path' => storage_path('logs/laravel.log'),
+            'path' => storage_path('logs/laravel-api-{date}.log'),
             'level' => env('LOG_LEVEL', 'debug'),
-            'days' => env('LOG_DAILY_DAYS', 14),
+            // Dias que se guarda 
+            'days' => env('LOG_DAILY_DAYS', 7),
             'replace_placeholders' => true,
         ],
 
